884c8666ce8a85accc352f3d1a4dd1b306d7bb1b
Livio <livio.brami98@gmail.com>
2022-11-05 15:13:22 -0400

    First commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://liviobrami:week6lab@mongodbserver.4dkez8v.mongodb.net/book_store?retryWrites=true&w=majority"
+}
